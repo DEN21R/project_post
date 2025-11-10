329bceb740ec157df7b3a25dc038be4c230f06b6
DEN21R <denr@ukr.net>
2025-11-10 20:58:16 +0200

    adde header

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,4 +1,17 @@
+import mesk_logo from '../../assets/logo/mesk_logo.svg'
+import styles from './styles.module.css'
+
 function Header() {
-  return <div></div>
+  return (
+    <div className={styles.headerContainer}>
+      <img src={mesk_logo} alt="" />
+      <nav>
+        <a href="">Главная</a>
+        <a href="">Музыка</a>
+        <a href="">Сообщества</a>
+        <a href="">Друзья</a>
+      </nav>
+    </div>
+  )
 }
 export default Header
